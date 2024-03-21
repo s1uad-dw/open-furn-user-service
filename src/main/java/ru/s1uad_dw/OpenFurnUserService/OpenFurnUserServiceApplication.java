63cb26538b2a452f07b9d8349061261b6d0e4ba2
s1uad-dw <s1uad.dw@gmail.com>
2024-03-21 14:06:10 +0300

    adding initial files was ended succefully

---
+++ src/main/java/ru/s1uad_dw/OpenFurnUserService/OpenFurnUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.OpenFurnUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFurnUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFurnUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/s1uad_dw/OpenFurnUserService/OpenFurnUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.s1uad_dw.OpenFurnUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFurnUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
